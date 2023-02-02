b19466340dbed4bda496e54479873945ee1f9203
Steve <steven.magelowitz@gmail.com>
2023-02-01 16:55:16 -0800

    bubblesort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
 export default function bubble_sort(arr: number[]): void {
+    let end = arr.length - 1;
 
+    while (end > 0) {
+        for (let i = 0; i < end; i++) {
+            const v1 = arr[i];
+            const v2 = arr[i + 1];
+
+            if (v1 > v2) {
+                arr[i] = v2;
+                arr[i + 1] = v1;
+            }
+        }
+        end -= 1;
+    }
 }
